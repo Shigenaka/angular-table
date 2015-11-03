1e5484cd20cc3ef705b8b9d496feeaab61c83a96
Shigenaka <mshig19@uw.edu>
2015-11-03 09:18:01 -0800

    finished project

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var sortApp = angular.module('sortApp', []);
 // Define a controller "mainController" for you application, 
 // making sure to pass in both the $scope and $http variables
 sortApp.controller('mainController', function($scope, $http) {
+    $scope.sortType = '-salary';
 
 	// Use and $http.get request to get the salary data
 	$http.get('data/salaries.json').then(function(response) {
@@ -13,3 +14,6 @@ sortApp.controller('mainController', function($scope, $http) {
         console.log($scope.data);
     });
 });
+
+//{{}}: expression
+//ng-something: directive
